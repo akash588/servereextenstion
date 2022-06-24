f1016d3c6f0fe045d34fcdd04af76c6eaa07d673
Akash Shelar <79242986+akash588@users.noreply.github.com>
2022-06-24 09:57:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+
+const fetch = require("./fetchfollwers");
+
+const cors = require("cors");
+
+
+
+
+
+
+
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+app.use(cors());
+
+app.use(express.json({ limit: "125mb" }));
+
+
+const { Blob } = require('buffer') ;
+
+const port = 5000;
+
+
+app.get("/getdata", fetch.viewOne);  
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   document.getElementById('alertButton').addEventListener('click', startRequest);
+// });
+
+
+
+
+app.post("/shopify", async(req, res) => {
+
+    await shopify(req.body)
+ 
+});
+
+
+app.listen(port, async () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+ 
+
+
+  
+ 
+});
+
+
+
+
+
+
+
+
+
+
+
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
+module.exports = app;
